5c35f4750c88d2779d9c7adeb6e8724d64e61ad4
jay719 <javariab17@gmail.com>
2021-08-17 17:46:19 -0600

    made graph

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 
 import './App.css';
 import * as THREE from 'three';
-import {Canvas} from 'react-three-fiber';
-import asteroidImg from './assets/asteroid-real'
-import { Suspense } from 'react';
+import {Canvas, useLoader} from 'react-three-fiber';
+import asteroidImg from './assets/asteroid-real.png'
+import { Suspense, useCallback, useMemo } from 'react';
 import { Float16BufferAttribute } from 'three';
 
 function Points () {
   const imgTexture = useLoader(THREE.TextureLoader, asteroidImg)
+  //graphing using trig/ sin
+  let t = 0; //phase shift
+  let f = 0.002; //drives frequency
+  let a = 3; //amplitude
+  const graph = useCallback((x,z) => {
+    return Math.sin(f * (x ** 2 + z ** 2 + t)) * a;
+  }, [t,f,a])  //dependencies to change the graph
 
   // {x1, y1, z1, x2, y2, z2...} coordinates
-  const count = 100 // number of points on y axis
-  const sep = 3 //space between
-
+  const count = 100; // number of points on y axis
+  const sep = 3; //space between
   let positions = useMemo (() => {
     let positions = []
 
-    for (let x1 = 0; x1 < count, x1++){
-      for(let z1 = 0; z1 < count; z1++)
-      
+    for (let xi = 0; xi < count; xi++){
+      for(let zi = 0; zi < count; zi++){
+        let x = sep * (xi - count / 2);
+        let z = sep * (zi - count / 2);
+        let y = graph(x, z); //calling the graph to get y value
+        positions.push(x,y,z)
+      }
     }
     return new Float32Array(positions)
-  })
+  }, [count, sep])
+
+
   return (
     <points>
       <bufferGeometry attach="geometry">
